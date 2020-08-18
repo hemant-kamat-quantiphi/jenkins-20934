c7e10f8d81521bcfb59b9a68ad9632695acf1fdb
EC2 Default User <ec2-user@ip-172-31-54-140.ec2.internal>
2020-08-18 07:07:22 +0000

    Ínitial commit

---
+++ test.py
@@ -0,0 +1,18 @@
+''' BasicTestCase Code '''
+import unittest
+from hello import app
+
+
+class BasicTestCase(unittest.TestCase):
+    '''Test case Implementation using Class'''
+    def test_home(self):
+        '''Function to define test '''
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello World!')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
